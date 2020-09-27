f38b35c84235fe03f7c3163e2f563290ab4b5aa5
Rok Povsic <rok.povsic@clarifai.com>
2020-09-27 18:54:15 +0200

    Add get model, list models, post model outputs tests

--- Tests/clarifai-swift-grpcTests/clarifai_swift_grpcTests.swift
+++ Tests/clarifai-swift-grpcTests/clarifai_swift_grpcTests.swift
@@ -9,18 +9,39 @@ enum ClarifaiError: Error {
 }
 
 final class clarifai_swift_grpcTests: XCTestCase {
-    func testListModels() throws {
+
+    private let GENERAL_MODEL_ID = "aaa03c23b3724a16a56b629203edc62c"
+    private let DOG_IMAGE_URL = "https://samples.clarifai.com/dog2.jpeg"
+    private let NON_EXISTING_IMAGE_URL = "https://example.com/this-image-does-not-exist.jpg"
+
+    private var group: MultiThreadedEventLoopGroup?
+    private var channel: ClientConnection?
+    private var client: Clarifai_Api_V2Client?
+
+    override func setUp() {
+        super.setUp()
+
         let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        let channel = ClientConnection.insecure(group: group).connect(host: "api-grpc.clarifai.com", port: 18080)
-        defer {
-            try? group.syncShutdownGracefully()
+        let channel = ClientConnection.secure(group: group).connect(host: "api.clarifai.com", port: 443)
+
+        self.group = group
+        self.channel = channel
+
+        let customMetadata: HPACKHeaders
+        do {
+            customMetadata = try authHeaders()
+        } catch ClarifaiError.runtimeError(let message) {
+            XCTFail(message)
+            return
+        } catch {
+            XCTFail("Unknown error")
+            return
         }
 
-        let client = Clarifai_Api_V2Client(channel: channel)
-        defer {
-            try? client.channel.close().wait()
-        }
+        self.client = Clarifai_Api_V2Client(channel: channel, defaultCallOptions: CallOptions(customMetadata: customMetadata))
+    }
 
+    private func authHeaders() throws -> HPACKHeaders {
         let apiKey = ProcessInfo.processInfo.environment["CLARIFAI_API_KEY"];
         if apiKey == nil {
             throw ClarifaiError.runtimeError("Please set the CLARIFAI_API_KEY environment variable.")
@@ -29,20 +50,116 @@ final class clarifai_swift_grpcTests: XCTestCase {
         var headers: HPACKHeaders = [:]
         headers.add(name: "authorization", value: "Key " + apiKey!)
 
-        let call = client.listModels(Clarifai_Api_ListModelsRequest(), callOptions: CallOptions(customMetadata: headers))
-        let listModelsResponse: Clarifai_Api_MultiModelResponse
-        do {
-            listModelsResponse = try call.response.wait()
-        } catch {
-            print("The RPC failed! \(error)")
-            return
+        return headers
+    }
+
+    override func tearDown() {
+        if let channel = self.channel {
+            XCTAssertNoThrow(try channel.close().wait())
+        }
+        if let group = self.group {
+            XCTAssertNoThrow(try group.syncShutdownGracefully())
         }
 
-        XCTAssertEqual(listModelsResponse.status.code, Clarifai_Api_Status_StatusCode.success)
-        XCTAssertNotEqual(listModelsResponse.models.count, 0)
+        super.tearDown()
+    }
+
+    func testGetModel() throws {
+        let response = try client!.getModel(Clarifai_Api_GetModelRequest.with {$0.modelID = GENERAL_MODEL_ID}).response.wait()
+        try raiseOnFailure(response.status)
+        XCTAssertEqual(response.model.id, GENERAL_MODEL_ID)
+    }
+
+    func testListModelsWithPagination() throws {
+        let response1 = try client!.listModels(Clarifai_Api_ListModelsRequest.with {$0.perPage = 2}).response.wait()
+        try raiseOnFailure(response1.status)
+        XCTAssertEqual(response1.models.count, 2)
+
+        // We shouldn't have 1000*500 number of models, so the result should be empty.
+        let response2 = try client!.listModels(Clarifai_Api_ListModelsRequest.with {$0.page = 1000; $0.perPage = 500}).response.wait()
+        try raiseOnFailure(response2.status)
+        XCTAssertEqual(response2.models.count, 0)
+    }
+
+    func testPostModelOutputs() throws {
+        let response = try client!.postModelOutputs(
+            Clarifai_Api_PostModelOutputsRequest.with {
+                $0.modelID = GENERAL_MODEL_ID;
+                $0.inputs = [
+                    Clarifai_Api_Input.with {
+                        $0.data = Clarifai_Api_Data.with {
+                            $0.image = Clarifai_Api_Image.with {
+                                $0.url = DOG_IMAGE_URL
+                            }
+                        }
+                    }
+                ]
+            }
+        ).response.wait()
+
+        try raiseOnFailure(response.status)
+        XCTAssertNotEqual(response.outputs[0].data.concepts.count, 0)
+    }
+
+    func testFailedPostModelOutputs() throws {
+        let response = try client!.postModelOutputs(
+            Clarifai_Api_PostModelOutputsRequest.with {
+                $0.modelID = GENERAL_MODEL_ID;
+                $0.inputs = [
+                    Clarifai_Api_Input.with {
+                        $0.data = Clarifai_Api_Data.with {
+                            $0.image = Clarifai_Api_Image.with {
+                                $0.url = NON_EXISTING_IMAGE_URL
+                            }
+                        }
+                    }
+                ]
+            }
+        ).response.wait()
+
+        XCTAssertEqual(response.status.code, Clarifai_Api_Status_StatusCode.failure)
+        XCTAssertEqual(response.outputs[0].status.code, Clarifai_Api_Status_StatusCode.inputDownloadFailed)
+    }
+
+    func testMixedSuccessPostModelOutputs() throws {
+        let response = try client!.postModelOutputs(
+            Clarifai_Api_PostModelOutputsRequest.with {
+                $0.modelID = GENERAL_MODEL_ID;
+                $0.inputs = [
+                    Clarifai_Api_Input.with {
+                        $0.data = Clarifai_Api_Data.with {
+                            $0.image = Clarifai_Api_Image.with {
+                                $0.url = DOG_IMAGE_URL
+                            }
+                        }
+                    },
+                    Clarifai_Api_Input.with {
+                        $0.data = Clarifai_Api_Data.with {
+                            $0.image = Clarifai_Api_Image.with {
+                                $0.url = NON_EXISTING_IMAGE_URL
+                            }
+                        }
+                    },
+                ]
+            }
+        ).response.wait()
+
+        XCTAssertEqual(response.status.code, Clarifai_Api_Status_StatusCode.mixedStatus)
+        XCTAssertEqual(response.outputs[0].status.code, Clarifai_Api_Status_StatusCode.success)
+        XCTAssertEqual(response.outputs[1].status.code, Clarifai_Api_Status_StatusCode.inputDownloadFailed)
+    }
+
+    private func raiseOnFailure(_ status: Clarifai_Api_Status_Status) throws {
+        if status.code != Clarifai_Api_Status_StatusCode.success {
+            throw ClarifaiError.runtimeError("Unexpected failure response \(status)")
+        }
     }
 
     static var allTests = [
-        ("testListModels", testListModels),
+        ("testGetModel", testGetModel),
+        ("testListModelsWithPagination", testListModelsWithPagination),
+        ("testPostModelOutputs", testPostModelOutputs),
+        ("testFailedPostModelOutputs", testFailedPostModelOutputs),
+        ("testMixedSuccessPostModelOutputs", testMixedSuccessPostModelOutputs),
     ]
 }
